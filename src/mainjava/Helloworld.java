d32ca5644bd3f33daf81af8185b896d72037c232
Krishna-dev870 <kateashishsunil@gmail.com>
2022-12-19 18:18:19 +0530

    First commit

---
+++ src/mainjava/Helloworld.java
@@ -0,0 +1,12 @@
+package mainjava;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello world");
+		System.out.println("This is the second line of code");
+	}
+
+}
